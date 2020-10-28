bb5e0d435d8086e095bb456edc2f84beb9a321a5
pc112 <saminali500@gmail.com>
2020-10-28 11:51:28 +0530

    Implemented features #7
    Supported to multiple items select.
    
    1. Can be a button or a "Tick" Icon, when done.
    2. Can be predefined & restrict "How many item can be chosen"
    3. Chosen list should remain intact when Recyclerview Filter items

--- app/src/main/java/com/minibugdev/sheetselection/demo/MainActivity.kt
+++ app/src/main/java/com/minibugdev/sheetselection/demo/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.minibugdev.sheetselection.demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.minibugdev.sheetselection.SheetSelection
 import com.minibugdev.sheetselection.SheetSelectionItem
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,22 +19,35 @@ class MainActivity : AppCompatActivity() {
 
         buttonDefaultSheetSelection.setOnClickListener {
             val items = listOf(
-                SheetSelectionItem("1", "Item #1", R.drawable.ic_extension),
-                SheetSelectionItem("2", "Item #2", R.drawable.ic_nature),
-                SheetSelectionItem("3", "Item #3", R.drawable.ic_fingerprint),
-                SheetSelectionItem("4", "Item #4", R.drawable.ic_face),
-                SheetSelectionItem("5", "Item #5", R.drawable.ic_extension),
-                SheetSelectionItem("6", "Item #6", R.drawable.ic_fingerprint)
+                SheetSelectionItem("1", "Item #1", false, R.drawable.ic_extension),
+                SheetSelectionItem("2", "Item #2", false, R.drawable.ic_nature),
+                SheetSelectionItem("3", "Item #3", false, R.drawable.ic_fingerprint),
+                SheetSelectionItem("4", "Item #4", false, R.drawable.ic_face),
+                SheetSelectionItem("5", "Item #5", true, R.drawable.ic_extension),
+                SheetSelectionItem("6", "Item #6", false, R.drawable.ic_fingerprint)
             )
 
             SheetSelection.Builder(this)
                 .title("Sheet Selection")
                 .items(items)
-                .selectedPosition(2)
+                //This will enable multiple Item choose feature
+                .forMultipleSelection(true)
+                //Maximum item choose limit
+                .maxItemChooseCount(2)
+                // Also you can use this
+                // .maxItemChooseCount(2, Max Item reached)
                 .showDraggedIndicator(true)
                 .searchEnabled(true)
-                .onItemClickListener { item, position ->
-                    textview.text = "You selected `${item.value}`, At position [$position]."
+                .onCompleteListener {
+                    if (it.isEmpty())
+                        return@onCompleteListener
+
+                    // Do what ever you want with these selected items.
+                    Log.e(TAG, "OnCompleteSelection: $it")
+                    for (sheetSelectionItem in it) {
+                        Log.e(TAG, "OnCompleteSelection: ${sheetSelectionItem.value}")
+                    }
+                    textview.text = "You selected ${it[0].value}."
                 }
                 .show()
         }
@@ -43,18 +61,30 @@ class MainActivity : AppCompatActivity() {
                         SheetSelectionItem(
                             key = "key_$it",
                             value = "Custom $it",
+                            isChecked = Random().nextBoolean(),
                             icon = R.drawable.ic_motorcycle
                         )
                     }
                 )
-                .selectedPosition(2)
+                //.selectedPosition(2)
                 .showDraggedIndicator(true)
                 .searchEnabled(true)
+                .forMultipleSelection(true)
                 .searchNotFoundText("Nothing!!")
                 .theme(R.style.Theme_Custom_SheetSelection)
-                .onItemClickListener { item, position ->
-                    textview.text = "You selected `${item.value}`, At position [$position]."
+                .onCompleteListener {
+                    if (it.isEmpty())
+                        return@onCompleteListener
+
+                    // Do what ever you want with these selected items.
+                    for (sheetSelectionItem in it) {
+                        Log.e(TAG, "OnCompleteSelection: $it")
+                    }
+                    textview.text = "You selected ${it[0].value}, At position"
                 }
+//                .onItemClickListener { item, position ->
+//                    textview.text = "You selected `${item.value}`, At position [$position]."
+//                }
                 .show()
         }
     }
